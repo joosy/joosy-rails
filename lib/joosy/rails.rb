77a431c188ee18ed1d3a8db0f8d6d6c6f81b7500
Boris Staal <boris@staal.io>
2013-06-24 02:18:10 +0700

    Tiny cleanup

--- lib/joosy/rails.rb
+++ lib/joosy/rails.rb
@@ -8,19 +8,6 @@ require 'joosy/rails/engine'
 
 module Joosy
   module Rails
-    # def self.resources(namespaces=nil)
-    #   predefined = {}
-    #   filtered_resources = Joosy::Rails::Engine.resources
-    #   if namespaces
-    #     namespaces = Array.wrap namespaces
-    #     filtered_resources = filtered_resources.select{|key, _| namespaces.include? key }
-    #   end
-    #   filtered_resources.each do |namespace, resources|
-    #     next unless resources && resources.size > 0
-    #     joosy_namespace = namespace.to_s.camelize.split('::').join('.')
-    #     predefined[joosy_namespace] = resources
-    #   end
-    #   predefined
-    # end
+
   end
 end
