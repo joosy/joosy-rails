2bc0c0b3881eae89067a65bc232042ed80b4f7ee
Andrew Shaydurov <gearhead@it-primorye.ru>
2013-08-06 01:23:28 +1100

    joosy_resources in routes.draw defines which resources will be autogenerated in Joosy

--- lib/joosy/rails/engine.rb
+++ lib/joosy/rails/engine.rb
@@ -3,26 +3,12 @@ require 'rails/engine'
 module Joosy
   module Rails
     class Engine < ::Rails::Engine
+      cattr_accessor :resources
+      self.resources = {}
+
       initializer 'joosy.extend.sprockets' do |app|
         Joosy.assets_paths.each{|p| app.assets.append_path p}
       end
-    end
-  end
-end
-
-module ActionDispatch::Routing
-  class Mapper
-    def joosy(route, options={})
-      extender = route.last == '/' ? '(*x)' : '(/*x)'
-
-      match route,
-        controller:   options[:controller] || 'joosy/rails/serve',
-        action:       options[:action] || 'index',
-        via:          :get, 
-        as:           (options[:application] ? "joosy_#{options[:application]}" : "joosy"),
-        defaults:     {route: route, application: options[:application]},
-        anchor:       false,
-        format:       false
     end
   end
 end
---
+++ lib/joosy/rails/routing.rb
@@ -0,0 +1,43 @@
+require 'action_dispatch/routing/mapper'
+
+module ActionDispatch::Routing
+  class Mapper
+    module Resources
+      class JoosyResource
+        def initialize(context)
+          @context = context
+        end
+
+        def method_missing(name, *args, &block)
+          @context.send(name, *args, &block)
+        end
+
+        def resources(*args, &block)
+          puts args.inspect
+          @context.resources(*args) do
+            scope = @context.instance_eval{ @scope }
+            namespace = Joosy::Rails::Engine.resources[scope[:module].to_s] ||= {}
+            namespace[args[0].to_s.singularize] ||= "#{scope[:path]}/#{args[0]}"
+            @context.joosy_resources(&block)
+          end
+        end
+      end
+
+      def joosy_resources(&block)
+        JoosyResource.new(self).instance_eval(&block) if block_given?
+      end
+    end
+
+    def joosy(route, options={})
+      extender = route.last == '/' ? '(*x)' : '(/*x)'
+
+      get route,
+        controller:   options[:controller] || 'joosy/rails/serve',
+        action:       options[:action] || 'index',
+        as:           (options[:application] ? "joosy_#{options[:application]}" : "joosy"),
+        defaults:     {route: route, application: options[:application]},
+        anchor:       false,
+        format:       false
+    end
+  end
+end
